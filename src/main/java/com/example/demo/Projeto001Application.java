3058b0a94d20b53969ec9ab0891b7cc5ed11e5f7
Lucas Couto <81658256+LukinhaCouto@users.noreply.github.com>
2022-10-27 12:45:15 -0300

    Category in memory

--- src/main/java/com/example/demo/Projeto001Application.java
+++ src/main/java/com/example/demo/Projeto001Application.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projeto001Application {
+import com.example.demo.entities.Category;
+import com.example.demo.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class Projeto001Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto001Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
 	}
 
 }
--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -1,32 +1,35 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Category;
+import com.example.demo.repositories.CategoryRepository;
 
 
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List <Category> list = new ArrayList<>();
-		list.add(new Category(1l, "Eletronics"));
-		list.add(new Category(2l, "Books"));
+		List <Category> list = categoryRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
-	/*@GetMapping(value = "/{id}")
-	public ResponseEntity<...> findById(@PathVariable Long id) {
-
-		return ResponseEntity.ok().body(...);
-	}*/
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category cat = categoryRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
 }
